b9ebbcde3abe4af07faf355954f9e013dab69472
AjayUppari <ajayuppari02@gmail.com>
2024-02-28 18:23:50 +0530

    Sidebar Implemented

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import SideBar from '@/components/shared/Sidebar'
 
 const Layout = ({children} : { children : React.ReactNode}) => {
   return (
     <main className='auth'>
+
+      <SideBar />
+      {/* <MobileNav /> */}
+
+
         <div className='root-container'>
             <div className='wrapper'>
                 {children}
